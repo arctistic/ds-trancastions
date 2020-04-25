fb4d8f63b4dede5e7a963e9d41faa6d17a2d3213
akhilRatnaChettri <chettri.akhilratna@gmail.com>
2020-04-25 15:55:56 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+app.use('/', require('./routes'));
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error: ${err}`);
+    }
+    console.log(`Server running on port: ${port}`);
+})
