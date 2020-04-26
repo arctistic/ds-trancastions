9c906c5f1e4c27b4c2cb4e77b645f331eb6e934a
akhilRatnaChettri <chettri.akhilratna@gmail.com>
2020-04-26 14:07:23 +0530

    Set Working and design

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,15 +2,8 @@ const Customer = require('../models/customer');
 
 module.exports.home = function(req, res){
     return res.render('home', {
-        title: "This is the title",
-        show_enquiry: 'false',
-        show_deposit: 'false',
-        show_transaction_update: 'false'
+        response: {}
     });
-}
-
-module.exports.signin = function(req, res){
-    return res.render('signin');
 }
 
 module.exports.signup = function(req, res){
@@ -19,7 +12,6 @@ module.exports.signup = function(req, res){
 
 module.exports.create = function(req, res){
     Customer.create(req.body);
-    console.log(req.body);
     return res.redirect('/');
 }
 
@@ -34,24 +26,21 @@ module.exports.enquiry = async function(req, res){
     
     if(customer.passcode == req.body.passcode){
         return res.render('home', {
-            show_enquiry: 'true',
-            show_deposit: 'false',
-            show_transaction_update: 'false',
-            customer: currentCustomer,
+            response: {
+                customer: currentCustomer,
+                show_enquiry: 'true'
+            }
         });
-    }else{
-
     }
     return res.redirect('/');
 }
 
 module.exports.deposit = async function(req, res){
-    
     let customer = await Customer.findOne({accountNo: req.body.accountNo});
     if(customer && customer.passcode == req.body.passcode){
         console.log("User found with correct passcode");
         customer.balance = parseInt(customer.balance) + parseInt(req.body.deposit);
-        customer.save();
+        await customer.save();
 
         let currentCustomer = {
             accountNo: customer.accountNo,
@@ -60,44 +49,91 @@ module.exports.deposit = async function(req, res){
         }
 
         return res.render('home', {
-            title: "This is the title",
-            show_enquiry: 'false',
-            show_deposit: 'true',
-            show_transaction_update: 'false',
-            customer : currentCustomer
+            response: {
+                show_deposit: 'true',
+                customer: currentCustomer
+            }
+        });
+    }
+    return res.redirect('back');
+}
+
+module.exports.withdraw = async function(req, res){
+    let customer = await Customer.findOne({accountNo: req.body.accountNo});
+    
+    if(customer && customer.passcode == req.body.passcode){
+        
+        if(parseInt(req.body.withdraw) > parseInt(customer.balance)){
+            console.log("Insufficient");
+            return res.render('home', {
+                response :{
+                    withdraw: 'insufficient',
+                    customer: {
+                        name: customer.name,
+                        balance: customer.balance,
+                        accountNo: customer.accountNo
+                    }
+                }
+            });
+        }
+
+        customer.balance = parseInt(customer.balance) - parseInt(req.body.withdraw);
+        await customer.save();
+        let currentCustomer = {
+            accountNo: customer.accountNo,
+            name: customer.name,
+            balance: customer.balance
+        }
+        console.log(currentCustomer);
+        return res.render('home', {
+            response: {
+                show_withdraw: 'true',
+                customer: currentCustomer
+            }
         });
     }
     return res.redirect('back');
 }
 
 module.exports.transaction = async function(req, res){
-    let sender = await Customer.findOne({accountNo: req.body.senderAccNo});
-    let receiver = await Customer.findOne({accountNo: req.body.receiverAccNo});
+    try {
+        let sender = await Customer.findOne({accountNo: req.body.senderAccNo});
+        let receiver = await Customer.findOne({accountNo: req.body.receiverAccNo});
+        
+        if(parseInt(req.body.amount) > parseInt(sender.balance)){
+            console.log("Insufficient");
+            return res.render('home', {
+                response :{
+                    withdraw: 'insufficient',
+                    customer: {
+                        name: sender.name,
+                        balance: sender.balance,
+                        accountNo: sender.accountNo
+                    }
+                }
+            });
+        }
+        
+        sender.balance = parseInt(sender.balance) - parseInt(req.body.amount);
+        receiver.balance = parseInt(receiver.balance) + parseInt(req.body.amount);
 
-    sender.balance = parseInt(sender.balance) - parseInt(req.body.amount);
-    receiver.balance = parseInt(receiver.balance) + parseInt(req.body.amount);
+        await sender.save();
+        await receiver.save();
 
-    sender.save();
-    receiver.save();
+        let currentCustomer = {
+            accountNo: sender.accountNo,
+            name: sender.name,
+            balance: sender.balance
+        }
 
-    let senderCustomer = {
-        accountNo: sender.accountNo,
-        name: sender.name,
-        balance: sender.balance
+        return res.render('home', {
+            response:{
+                show_transaction_update: 'true',
+                customer: currentCustomer,
+            }
+        });
+        
+    } catch (err) {
+        console.log("Error in controller: ",err)
     }
-    let reveicerCustomer = {
-        accountNo: receiver.accountNo,
-        name: receiver.name,
-    }
-
-    // console.log(sender);
-    // console.log(receiver);
-
-    return res.render('home', {
-        show_enquiry: 'false',
-        show_deposit: 'false',
-        show_transaction_update: 'true',
-        sender: senderCustomer,
-        receiver: reveicerCustomer
-    });
 }
--- index.js
+++ index.js
@@ -7,7 +7,16 @@ app.use(express.static('./assets'));
 app.use(express.urlencoded());
 app.set('views', './views');
 app.set('view engine', 'ejs');
-app.use('/', require('./routes'));
+
+const controller = require('./controllers/controller');
+
+app.get('/', controller.home);
+app.get('/signup', controller.signup);
+app.post('/create-customer', controller.create);
+app.post('/enquiry', controller.enquiry);
+app.post('/deposit', controller.deposit);
+app.post('/withdraw', controller.withdraw);
+app.post('/transaction', controller.transaction);
 
 app.listen(port, function(err){
     if(err){
